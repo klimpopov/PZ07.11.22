97676631e4bcf5537c77a4281b63e55113311969
Klim <naiberd669@bk.ru>
2022-11-07 21:49:40 +0300

    Написал задачу 30

---
+++ Task_30/Program.cs
@@ -0,0 +1,14 @@
+﻿// Выводит массив из 8 элементов, заполняемый нулями и единицами в случайном порядке.
+
+int[] array = GetBinaryArray(8);
+Console.WriteLine($"[{String.Join(", ", array)}]");
+
+int[] GetBinaryArray(int size)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(0, 2);
+    }
+    return result;
+}

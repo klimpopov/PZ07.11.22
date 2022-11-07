221abc746a8e5b5f213c97e32737c698cc71a424
Klim <naiberd669@bk.ru>
2022-11-07 21:12:10 +0300

    рабочая функция

---
+++ Task_24/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Принимаем на вход число (А) и выдает сумму чисел от 1 до А
+*/
+void GetSumNums(int number)
+{
+    int sum = 0;
+    for (int i = 1; i <= Math.Abs(number); i++) // Math.Abs число по модулю
+        sum = sum + i;
+    Console.WriteLine($"Сумма цифр от 1 до {number} = {sum}");
+}
+Console.Clear();
+Console.WriteLine("Введите число");
+GetSumNums(Convert.ToInt32(Console.ReadLine()));
+
+Console.WriteLine("Введите число");
+GetSumNums(Convert.ToInt32(Console.ReadLine()));
